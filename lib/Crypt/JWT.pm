8a814e355d9dcead067b4e41803809b804f0f00a
Karel Miko <miko@dcit.cz>
2016-05-04 07:46:21 +0200

    v0.014

--- lib/Crypt/JWT.pm
+++ lib/Crypt/JWT.pm
@@ -3,7 +3,7 @@ package Crypt::JWT;
 use strict;
 use warnings;
 
-our $VERSION = '0.013';
+our $VERSION = '0.014';
 
 use Exporter 'import';
 our %EXPORT_TAGS = ( all => [qw(decode_jwt encode_jwt)] );
