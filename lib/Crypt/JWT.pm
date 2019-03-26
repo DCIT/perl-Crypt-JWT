5f6259eeaf24bf377e279abf0c08f8b0727d37f6
Karel Miko <karel.miko@gmail.com>
2019-03-26 11:40:40 +0100

    update Changes

--- lib/Crypt/JWT.pm
+++ lib/Crypt/JWT.pm
@@ -3,7 +3,7 @@ package Crypt::JWT;
 use strict;
 use warnings;
 
-our $VERSION = '0.023';
+our $VERSION = '0.024';
 
 use Exporter 'import';
 our %EXPORT_TAGS = ( all => [qw(decode_jwt encode_jwt)] );
