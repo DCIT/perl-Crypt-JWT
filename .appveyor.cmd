eed0da9ad00a7866cea32c22f6ce7c4d04cb8d1b
Karel Miko <karel.miko@gmail.com>
2025-01-25 17:10:01 +0100

    tuning github workflows

--- .appveyor.cmd
+++ .appveyor.cmd
@@ -14,6 +14,21 @@ if "%perl_type%" == "cygwin32" (
 ) else if "%perl_type%" == "cygwin64" (
   start /wait c:\cygwin64\setup-x86_64.exe -q -g -P perl -P make -P gcc -P gcc-g++ -P libcrypt-devel -P libnsl-devel
   set "PATH=C:\cygwin64\usr\local\bin;C:\cygwin64\bin;%PATH%"
+) else if "%perl_type%" == "strawberry_gh" (
+  if "%perl_version%" == "5.40" (
+    echo Going to download "%download_url%"
+    wget -nv https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_54001_64bit_UCRT/strawberry-perl-5.40.0.1-64bit-portable.zip -O downloaded-strawberry.zip
+  ) else if "%perl_version%" == "5.38" (
+    echo Going to download "%perl_version%"
+    wget -nv https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_53822_64bit/strawberry-perl-5.38.2.2-64bit-portable.zip -O downloaded-strawberry.zip
+  ) else if "%perl_version%" == "5.36" (
+    echo Going to download "%perl_version%"
+    wget -nv https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_53631_64bit/strawberry-perl-5.36.3.1-64bit-portable.zip -O downloaded-strawberry.zip
+  ) else (
+    echo FATAL unexpected value "%perl_version%"
+  )
+  7z x downloaded-strawberry.zip -oc:\spperl\
+  set "PATH=c:\spperl\perl\site\bin;c:\spperl\perl\bin;c:\spperl\c\bin;%PATH%"
 ) else if "%perl_type%" == "strawberry" (
   wget -q http://strawberryperl.com/download/%perl_version%/strawberry-perl-%perl_version%-%perl_bits%bit-portable.zip -O downloaded-strawberry.zip
   7z x downloaded-strawberry.zip -oc:\spperl\
